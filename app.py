ccbea68d727938d52434d65bc4cd8ca2778d1c9d
Ryan Tasson <l4zmq2r@lowhangingfru.it>
2019-09-28 00:58:26 -0400

    Create more utility functions

--- app.py
+++ app.py
@@ -1,92 +1,48 @@
-import os
-import sys
 import json
-import ffmpeg
 import os.path
-import tempfile
 
-from flask import Flask, request, Response, stream_with_context
+from flask import Flask, request, Response
 
-from util import error, get_parent_path, get_complete_path
+from util import error, get_complete_path, directory_listing, transcode
 
 app = Flask(__name__)
+
+try:
+    root = os.path.abspath(os.environ['MUSIC_DIR'])
+except:
+    print("Must specify a MUSIC_DIR environment variable.")
+    exit(1)
 
 # TODO: Handle failure cases; see if path is safe to try to transcode
 @app.route('/play')
 def stream_file():
     if not request.args['path']:
         err_msg = "Not a streamable path: {}".format(requests.args['path'])
-        print(err_msg)
         return error(err_msg)
-    path = get_complete_path(request.args['path'], root)
-    def generate():
-        handle, tmp = tempfile.mkstemp(prefix='djtaytay')
-        os.close(handle)
-        process = (
-            ffmpeg
-                .input(path)
-                .output(tmp, format='webm', acodec='libvorbis', ac=1, ar='196k')
-                .overwrite_output()
-                .run_async()
-        )
-        file = open(tmp, mode='rb')
-        while True:
-            data = file.read(128)
-            if not data:
-                process.poll()
-                if process.returncode == None:
-                    continue
-                else:
-                    file.close()
-                    os.remove(tmp)
-                    break
-            yield data
-            
-    return Response(stream_with_context(generate()), mimetype='audio/webm')
+
+    # Validate path and construct an absolute path
+    try:
+        path = get_complete_path(request.args['path'], root)
+    except Exception as e:
+        return error(str(e))
+
+    return Response(transcode(path), mimetype='audio/webm')
+
 
 @app.route("/browse")
 def file_listing():
-    path = get_complete_path(request.args.get("path", "/"), root)
+    # Validate path and construct an absolute path
     try:
-        # if the computed path is not in the root path...
-        if os.path.commonpath([path, root]) != root:
-            err_msg = "Path {} is not in {}".format(path, root)
-            print(err_msg)
-            return error(err_msg)
-    except:
-        # if paths contains both absolute and relative paths
-        err_msg = "Set MUSIC_DIR to an absolute path."
-        print(err_msg)
-        return error(err_msg)
-    if not os.path.isdir(path):
-        err_msg = "Not a directory: {}".format(path)
-        print(err_msg)
-        return error(err_msg)
-    listing = [
-      {
-        "name": "..",
-        "path": get_parent_path(path)[len(root):],
-        "type": "dir"
-      }
-    ]
-    try:
-        # TODO: omit hidden files
-        for i in os.scandir(path=path):
-            type = "file" if i.is_file() else "dir"
-            file = {
-                "name": i.name,
-                "path": i.path[len(root):],
-                "type": type
-            }
-            listing.append(file)
-        return json.dumps(listing)
+        path = get_complete_path(request.args.get("path", "/"), root)
     except Exception as e:
-        return json.dumps({"error": "Unknown error"}), 400
+        return error(str(e))
+
+    try:
+        listing = json.dumps(directory_listing(path))
+        return Response(listing, mimetype='application/json')
+    except Exception as e:
+        return error(str(e))
+
 
 if __name__ == '__main__':
-    if not os.environ.get('MUSIC_DIR'):
-        print("Must specify a MUSIC_DIR environment variable.")
-        exit(1)
-    global root
-    root = os.environ.get('MUSIC_DIR').rstrip('/')
     app.run(debug=True)
--- util.py
+++ util.py
@@ -1,14 +1,110 @@
-import os
 import json
+import ffmpeg
+import os.path
+import tempfile
+
+from flask import Response
+
+try:
+    root = os.path.abspath(os.environ['MUSIC_DIR'])
+except:
+    print("Must specify a MUSIC_DIR environment variable.")
+    exit(1)
 
 def error(message):
-    return json.dumps({"error": message}), 400
+    """error(message)
 
-def get_parent_path(path):
-    return os.path.dirname(path)
+    Helper function to print to stdout and return `message` in
+    a JSON blob in a Flask Response.
+
+    """
+    # TODO: use Flask's logging
+    print("Error: {}".format(message))
+    response = json.dumps({"error": message})
+    return Response(response, 400, mimetype='application/json')
 
 def get_complete_path(path, root):
-    r = root
-    r += "/"
-    r += path.lstrip('/')
-    return r
+    """get_complete_path(path, root)
+
+    This function generates an absolute path from the base directory
+    and the provided path and validates that it is within the base
+    directory. It returns the complete path as a string if successful
+    and raises a ValueError if unsuccessful.
+
+    """
+    raw_path = root + "/" + path.lstrip('/')
+    abs_path = os.path.abspath(raw_path)
+
+    # if the computed path is not in the root path...
+    if os.path.commonpath([abs_path, root]) != root:
+        err_msg = "Path {} is not in {}".format(path, root)
+        ex = ValueError()
+        Ex.strerror = err_msg
+        raise ex
+
+    return abs_path
+
+def transcode(path):
+    """transcode(path)
+
+    This function is a generator. It will asyncronously begin transcoding
+    the file at `path` into a temporary file and then yield the contents
+    of that file 128 bytes at a time. If there is nothing to read,
+    the transcoder is checked to see if it has exited, and if it has
+    the function returns.
+
+    """
+    handle, tmp = tempfile.mkstemp(prefix='djtaytay')
+    os.close(handle)
+    process = (
+        ffmpeg
+            .input(path)
+            .output(tmp, format='webm', acodec='libvorbis', ac=1, ar='196k')
+            .overwrite_output()
+            .run_async()
+    )
+    file = open(tmp, mode='rb')
+    while True:
+        data = file.read(128)
+        if not data:
+            process.poll()
+            if process.returncode == None:
+                continue
+            else:
+                file.close()
+                os.remove(tmp)
+                break
+        yield data
+
+def directory_listing(path):
+    """directory_listing(path)
+
+    This function constructs a dict representing a directory's
+    contents and returns it. If the path is not a directory,
+    a ValueError is raised.
+
+    """
+    if not os.path.isdir(path):
+        ex = ValueError()
+        ex.strerror = "Not a directory: {}".format(path)
+        raise ex
+
+    listing = [
+      {
+        "name": "..",
+        "path": os.path.dirname(path)[len(root):],
+        "type": "dir"
+      }
+    ]
+
+    # TODO: omit hidden files
+    for i in os.scandir(path=path):
+        type = "file" if i.is_file() else "dir"
+        file = {
+            "name": i.name,
+            "path": i.path[len(root):],
+            "type": type
+        }
+        listing.append(file)
+    return listing
+
